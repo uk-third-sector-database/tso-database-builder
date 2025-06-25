5a265e18c8ef5b4af53665c4be98b4be887e63cc
Fiona CK <fionacouperkenney@gmail.com>
2025-06-25 17:33:20 +0100

    update prepare_zip to include finhist

--- prepare_zip.sh
+++ prepare_zip.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
+# prepare zipped spine files:
+
 jupyter nbconvert --to pdf --execute visualise/stats_and_visuals.ipynb
 pdflatex builder.tex
 zip tso-spine-files.zip ../public_spine_data/public_spine.*csv builder.pdf  -j
 git add tso-spine-files.zip
-git commit -m 'updated zip file for downloads'
-git push
+git commit -m 'updated spine zip file for downloads'
+git push origin new-build-spine
+
+
+# prepare zipped financial history files:
+
+zip finhist_files.zip ../processed_data/public_spine.finhist.csv -j
+git add finhist_files.zip
+git commit -m 'updated finhist zip for downloads'
+git push origin new-build-spine
